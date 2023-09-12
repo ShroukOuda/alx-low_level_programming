01ce75bc6b2904a578c852fcf1399ac175ab6c2d
ShroukOuda <shroukouda2@gmail.com>
2023-09-12 08:14:27 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,16 @@
  */
 int print_last_digit(int n)
 {
-	int digit = n % 10;
+	int digit;
 
+	if (n < 0)
+	{
+		digit = -1 * (n % 10);
+	}
+	else
+	{
+		digit = n % 10;
+	}
 	_putchar(digit + 48);
 	return (digit);
 }
