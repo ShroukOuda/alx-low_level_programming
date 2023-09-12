da8a16b295fbd8df31d6aa67417c26c42abf23b2
ShroukOuda <shroukouda2@gmail.com>
2023-09-12 07:54:31 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - check the code.
+ * @n: integer
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+	int a = n;
+
+	if (a < 0)
+	{
+		return (a * -1);
+	}
+	else
+		return (a);
+}
+
