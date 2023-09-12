c86df1a883136cf5c78c76c1e07c5fb18b89aa51
ShroukOuda <shroukouda2@gmail.com>
2023-09-12 16:50:15 -0700

    commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int i, n1 = 1, n2 = 2, sum = 0;
+
+	printf("%ld, %ld, ", n1, n2);
+	for (i = 3; i <= 98; i++)
+	{
+		sum = n1 + n2;
+		n1 = n2;
+		n2 = sum;
+		printf("%ld", sum);
+		if (i == 98)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
