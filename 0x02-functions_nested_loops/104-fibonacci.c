3292075b0865b8536a1568548f0110a80b5f7848
ShroukOuda <shroukouda2@gmail.com>
2023-09-12 18:00:24 -0700

    commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,32 +6,27 @@
  */
 int main(void)
 {
-	long int i, n = 1, m = 2, sum = 0, n1, n2, m1, m2, s, c;
+	long int i, n = 1, m = 2, n1, n2, m1, m2;
 
 	printf("%ld, %ld, ", n, m);
 	for (i = 3; i <= 91; i++)
 	{
-		sum = n + m;
-		n = m;
-		m = sum;
-		printf("%ld, ", sum);
+		n = n + m;
+		m = n - m;
+		printf("%ld, ", n);
 	}
 	n1 = n / 1000000000;
 	n2 = n % 1000000000;
 	m1 = m / 1000000000;
 	m2 = m % 1000000000;
-	s = sum;
-	c = sum;
 	for (i = 92; i < 99; i++)
 	{
-		s = n1 + m1;
-		n1 = m1;
-		m1 = s;
-		printf("%ld", s);
-		c = n2 + m2;
-		n2 = m2;
-		m2 = c;
-		printf("%ld", c);
+		printf("%ld", n1);
+		n1 = n1 + m1;
+		m1 = n1 - m1;
+		printf("%ld", n2);
+		n2 = n2 + m2;
+		m2 = n2 - m2;
 		if (i == 98)
 		{
 			printf("\n");
