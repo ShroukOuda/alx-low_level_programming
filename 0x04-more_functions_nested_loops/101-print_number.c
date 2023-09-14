a43548475af3adbd63c6b4f285bff1e17dc16ce2
ShroukOuda <shroukouda2@gmail.com>
2023-09-14 13:25:21 -0700

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,31 +1,21 @@
 #include "main.h"
 /**
-* print_number - check the code.
-* @n: is an integer
-* Return: Always 0.
-*/
+ * print_number - check the code.
+ * @n: is an integer
+ * Return: Always 0.
+ */
 void print_number(int n)
 {
 	int num = n;
 
-	while (num > 0)
+	if (num < 0)
 	{
-	if (num > 9 && num < 100)
+		_putchar('-');
+		num = -num;
+	}
+	if (num / 10 > 0)
 	{
-_putchar((num / 10) + 48);
+		print_number(num / 10);
 	}
-	else if (num > 99 && num < 1000)
-	{
-		_putchar((num / 100) + 48);
-	}
-	else if (num > 999 && num < 10000)
-	{
-		_putchar((num / 1000) + 48);
-	}
-	else
-	{
-		_putchar((num % 10) + 48);
-	}
-	num = num / 10;
-	}
+	_putchar((num % 10) + 48);
 }
