de0f0582eca50d6d263a5b5c68b35a8b322c8b0a
ShroukOuda <shroukouda2@gmail.com>
2023-09-14 06:25:03 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - check the code.
+ * @a: is first integer
+ * @b: is second integer
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int n = a, m = b;
+
+	return (n * m);
+}
