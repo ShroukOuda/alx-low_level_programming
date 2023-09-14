4547ca1b10771f59ea896426a3bdcf39414a21a9
ShroukOuda <shroukouda2@gmail.com>
2023-09-14 07:22:40 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - check the code.
+ * @n: is integer
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int cnt = n, i, j;
+
+	if (cnt > 0)
+	{
+		for (i = 0; i < cnt; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
