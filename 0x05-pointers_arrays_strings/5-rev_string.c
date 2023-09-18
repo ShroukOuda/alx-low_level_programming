63a510eb4830a7d28029a07a5c229e842668c502
ShroukOuda <shroukouda2@gmail.com>
2023-09-18 11:30:43 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - check the code.
+ * @s: it's ptr
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int n = 0, i;
+	char c;
+
+	while (s[n])
+		n++;
+	for (i = 0; i < n / 2; i++)
+	{
+		c = s[i];
+		s[i] = s[n - 1 - i];
+		s[n - i] = c;
+	}
+}
+
