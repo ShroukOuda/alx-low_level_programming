3378f0450afe189aee36e45767c8625b53a0c5d9
ShroukOuda <shroukouda2@gmail.com>
2023-09-18 08:57:35 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - check the code.
+ * @str: it's ptr
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
