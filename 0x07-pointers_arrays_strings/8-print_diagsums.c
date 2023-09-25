08d389547ed1205eef2dbecd92b31953f092b22f
ShroukOuda <shroukouda2@gmail.com>
2023-09-25 16:37:50 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagsums - check the code.
+ * @a: it's a ptr
+ * @size: it's an int
+ * Return: Always 0.
+ */
+void print_diagsums(int *a, int size)
+{
+	long int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[size - i - 1];
+	a += 3;
+	}
+	printf("%ld, %ld\n", s1, s2);
+}
