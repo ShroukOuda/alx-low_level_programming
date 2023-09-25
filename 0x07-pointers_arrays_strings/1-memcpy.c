22caea416382d0e2623ffdd43a98eaa45d25d135
ShroukOuda <shroukouda2@gmail.com>
2023-09-25 13:25:24 -0700

    commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	while (n > 0)
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		*dest = *src;
-		dest++;
-		src++;
-		n--;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
