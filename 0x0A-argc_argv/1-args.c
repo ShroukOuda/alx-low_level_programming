ff92b1c21235c4b751ddcdde2bec37ba1b5b5565
ShroukOuda <shroukouda2@gmail.com>
2023-10-02 13:16:59 -0700

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code.
+ * @argc: it's argc
+ * Return: Always 0.
+ */
+int main(int argc, char **)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
