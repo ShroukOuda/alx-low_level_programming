b4910e5db2e441805affd34993f0c9d9e3505047
ShroukOuda <shroukouda2@gmail.com>
2023-10-02 13:03:04 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code.
+ * @argc: it's argc
+ * @argv: it's argv
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
+}
