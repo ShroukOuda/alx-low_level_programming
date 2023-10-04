f36f99d1cd9df56a892b17e89a6d01b8245f75d7
ShroukOuda <shroukouda2@gmail.com>
2023-10-04 15:20:29 -0700

    commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,18 +1,20 @@
 #include "main.h"
 /**
- * _strdup - check the code.
- * @str: it's str
- * Return: Always 0.
- */
+* _strdup - check the code.
+* @str: it's str
+* Return: Always 0.
+*/
 char *_strdup(char *str)
 {
 	char *ptr;
 	int i, n = 0;
 
+	if (str == 0)
+		return (NULL);
 	while (str[n])
 		n++;
 	ptr = malloc(n);
-	if (ptr == 0 || str == 0)
+	if (ptr == 0)
 		return (NULL);
 	for (i = 0; i < n; i++)
 	{
