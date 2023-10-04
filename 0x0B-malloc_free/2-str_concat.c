5ded4fd574c4d4d462376e6332ea3a946fb1e2fe
ShroukOuda <shroukouda2@gmail.com>
2023-10-04 16:04:48 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,26 +8,43 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i, n = 0, m = 0;
-	char *p1;
+	char *p1, *p2, *result;
 
 	while (s1[n])
 		n++;
 	while (s2[m])
 		m++;
-	p1 = malloc(n + m + 1);
-	if (p1 == 0)
-	{
-		return (NULL);
-	}
+	p1 = malloc(n + 1);
 	for (i = 0; i < n; i++)
 	{
 		p1[i] = s1[i];
 	}
+	p2 = malloc(m + 1);
+	for (i = 0; i < m; i++)
+	{
+		p2[i] = s2[i];
+	}
+	result = malloc(n + m + 1);
+	for (i = 0; i < n; i++)
+	{
+		result[i] = p1[i];
+	}
 	for (i = n; i < (n + m); i++)
 	{
-		p1[i] = *s2;
-		s2++;
+		result[i] = *p2;
+		p2++;
 	}
-	return (p1);
-	free(p1);
+	if (p1 == 0 && p2 == 0)
+	{
+		return (NULL);
+	}
+	else if (p1 == 0 && p2 != 0)
+	{
+		return (p2);
+	}
+	else if (p1 != 0 && p2 == 0)
+	{
+		return (p1);
+	}
+	return (result);
 }
