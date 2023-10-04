70f023ddd7b99f15bc45007aa51312ae6b6ab27a
ShroukOuda <shroukouda2@gmail.com>
2023-10-04 15:07:20 -0700

    commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strdup - check the code.
+ * @str: it's str
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, n = 0;
+
+	while (str[n])
+		n++;
+	ptr = malloc(n);
+	if (ptr == NULL || str == NULL)
+		return (NULL);
+	for (i = 0; i < n; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
