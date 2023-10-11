8a691db0f71efb5c3b3e1ece82de0acc8ff8f8a2
ShroukOuda <shroukouda2@gmail.com>
2023-10-11 09:03:08 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ * struct dog - check the code.
+ * @name: it's name of dog
+ * @age: it's age of dog
+ * @owner: it's owner of dog
+ */
 #include <stdio.h>
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 
