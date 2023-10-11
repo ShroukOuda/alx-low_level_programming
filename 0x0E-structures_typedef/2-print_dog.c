b288e1f390681db5633ddc36b62a8b78ed56389c
ShroukOuda <shroukouda2@gmail.com>
2023-10-11 13:33:00 -0700

    commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - check the code.
+ * @d: it's a ptr
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: nil\n");
+		}
+		if (d->age)
+		{
+			printf("Age: %.6f\n", d->age);
+		}
+		else
+		{
+			printf("Age: nil\n");
+		}
+		if (d->owner)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: nil\n");
+		}
+	}
+}
+
