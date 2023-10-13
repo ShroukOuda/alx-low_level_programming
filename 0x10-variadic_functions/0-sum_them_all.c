d9a8323388de16ef3032d426a9760314ae1f0e67
ShroukOuda <shroukouda2@gmail.com>
2023-10-12 17:01:48 -0700

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,13 +8,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	unsigned i, sum = 0;
+	unsigned int i, sum = 0;
 
 	if (n == 0)
 		return (0);
 	va_start(args, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, unsigned);
+		sum += va_arg(args, unsigned int);
 	va_end(args);
 	return (sum);
 }
