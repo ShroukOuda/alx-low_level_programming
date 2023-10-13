bf982e95d81d55444acc8910d75a2851c97d2c9f
ShroukOuda <shroukouda2@gmail.com>
2023-10-12 17:00:01 -0700

    commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - check the code.
+ * @n: it's an unsigned int
+ * Return: Always 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned i, sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, unsigned);
+	va_end(args);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARADIC_H
+#define VARADIC_H
+
+#include <stdio.h>
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+
+
+#endif 
