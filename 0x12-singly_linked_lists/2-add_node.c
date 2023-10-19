c72b773f794c0ffa757135fd5923862b71767ce4
ShroukOuda <s36568961@gmail.com>
2023-10-19 11:24:51 -0700

    commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_node - check the code.
+ * @head: it's a head
+ * @str: it's a str
+ * Return: Always 0.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int i = 0;
+	list_t *newnode;
+
+	while (str[i])
+		i++;
+	newnode = malloc(sizeof(list_t));
+	newnode->str = strdup(str);
+	newnode->len = i;
+	newnode->next = (*head);
+	(*head) = newnode;
+	return ((*head));
+}
