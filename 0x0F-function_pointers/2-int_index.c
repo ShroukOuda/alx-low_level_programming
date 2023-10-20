8620d864225dd2efef9ca3ceb65be30805274257
ShroukOuda <s36568961@gmail.com>
2023-10-19 23:42:19 -0700

    commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_index - check the code.
+ * @array: it's an int
+ * @size: it's an int
+ * @cmp: it's a fptr
+ * Return: Always 0.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+		return (i);
+	}
+	return (0);
+}
