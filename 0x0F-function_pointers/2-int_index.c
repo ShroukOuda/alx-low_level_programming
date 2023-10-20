9ee136d1ae3c0829b5d0ebe1956f204a1a904852
ShroukOuda <s36568961@gmail.com>
2023-10-19 23:45:35 -0700

    commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,12 +10,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
+	if (array == NULL || size <= 0)
 		return (-1);
+	if (cmp == NULL)
+		return (0);
 	for (i = 0; i < size; i++)
 	{
 		if ((*cmp)(array[i]))
 		return (i);
 	}
-	return (0);
+	return (-1);
 }
