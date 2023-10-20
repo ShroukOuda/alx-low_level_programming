686179b1f763b01f2086702f259ac1f1f804c005
ShroukOuda <s36568961@gmail.com>
2023-10-19 22:54:48 -0700

    commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - check the code.
+ * @name: it's a string
+ * @f: it's a fptr
+ * Return: Always 0.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FPTR_H
+#define FPTR_H
+
+#include <stdio.h>
+
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif /* FPTR_H */
