45477592a1933b6ab78ac8d8ec513a367e60df86
ShroukOuda <s36568961@gmail.com>
2023-10-21 21:50:04 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,44 +7,31 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, n = 0, m = 0;
-	char *p1, *p2, *result;
+	int i = 0, n = 0, m = 0, k = 0;
+	char *result;
 
 	while (s1[n])
 		n++;
 	while (s2[m])
 		m++;
-	p1 = malloc(n + 1);
-	for (i = 0; i < n; i++)
-	{
-		p1[i] = s1[i];
-	}
-	p2 = malloc(m + 1);
-	for (i = 0; i < m; i++)
-	{
-		p2[i] = s2[i];
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s1 == NULL)
+		s2 = "";
 	result = malloc(n + m + 1);
-	for (i = 0; i < n; i++)
-	{
-		result[i] = p1[i];
-	}
-	for (i = n; i < (n + m); i++)
-	{
-		result[i] = *p2;
-		p2++;
-	}
-	if (p1 == 0 && p2 == 0)
-	{
+	if (result == NULL)
 		return (NULL);
-	}
-	else if (p1 == 0 && p2 != 0)
+	while (s1[i])
 	{
-		return (p2);
+		result[i] = s1[i];
+		i++;
 	}
-	else if (p1 != 0 && p2 == 0)
+	while (s2[k])
 	{
-		return (p1);
+		result[i] = s2[k];
+		i++;
+		k++;
 	}
+	result[i] = '\0';
 	return (result);
 }
