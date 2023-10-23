d204f383c2aed025efdeb6a04b211d6e935911c4
ShroukOuda <s36568961@gmail.com>
2023-10-23 07:22:30 -0700

    commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - check the code.
+ * @head: it's a head
+ * Return: Always 0.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *fre;
+
+	temp = *head;
+	*head = NULL;
+	while (temp != 0)
+	{
+		fre = temp;
+		temp = temp->next;
+		free(fre);
+	}
+}
