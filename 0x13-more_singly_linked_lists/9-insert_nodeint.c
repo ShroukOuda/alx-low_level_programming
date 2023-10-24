092b7d7f66bd05b95f07ab01f882869646adb241
ShroukOuda <s36568961@gmail.com>
2023-10-23 23:57:33 -0700

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,9 +12,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	newnode = malloc(sizeof(listint_t));
-	if (!newnode)
+	if (!newnode || !head)
 		return (NULL);
 	temp = *head;
+	if (*head == NULL)
+	{
+		newnode->n = n;
+		*head = newnode;
+		newnode->next = 0;
+	}
+	else
+	{
 	while (temp && (i < idx - 1))
 	{
 		temp = temp->next;
@@ -25,5 +33,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newnode->n = n;
 	newnode->next = temp->next;
 	temp->next = newnode;
+	}
 	return (newnode);
 }
