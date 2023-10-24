85690481d0b965d398d5a720b9af90561b9712a6
ShroukOuda <s36568961@gmail.com>
2023-10-23 22:06:03 -0700

    commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,23 +1,5 @@
 #include "lists.h"
 /**
- * list_len - check the code.
- * @head: it's a head
- * Return: Always 0.
- */
-unsigned int list_len(listint_t *head)
-{
-	unsigned int len = 0;
-	listint_t *temp;
-
-	temp = head;
-	while (temp)
-	{
-		temp = temp->next;
-		len++;
-	}
-	return (len);
-}
-/**
  * get_nodeint_at_index - check the code.
  * @head: it's a head
  * @index: it's an index
@@ -28,13 +10,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int i = 0;
 
-	if (index < 1 || index > list_len(head))
-		return (NULL);
 	temp = head;
-	while (i < index)
+	while (temp && (i < index))
 	{
 		temp = temp->next;
 		i++;
 	}
-	return (temp);
+	return (temp ? temp : NULL);
 }
