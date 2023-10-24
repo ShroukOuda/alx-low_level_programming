90ba3354166a53f21a7c629defc96b5d9c492ad5
ShroukOuda <s36568961@gmail.com>
2023-10-24 02:04:06 -0700

    commit

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - check the code.
+ * @head: it's a head
+ * @index: it's n index
+ * Return: Always 0.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp;
+	unsigned int i = 0;
+
+	if (*head == NULL || head == NULL)
+		return (-1);
+	temp = *head;
+	while (temp && (i < index - 1))
+	{
+		temp = temp->next;
+		i++;
+	}
+	temp->next = temp->next->next;
+	temp = temp->next;
+	free(temp);
+	return (temp ? 1 : -1);
+}
