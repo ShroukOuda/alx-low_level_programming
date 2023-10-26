afcac2c8514ae83867e8adc54fea1eb6a80411e4
ShroukOuda <s36568961@gmail.com>
2023-10-26 00:43:32 -0700

    commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * _pow - check the code.
+ * @n: it's a n
+ * @p: it's a p
+ * Return: Always 0.
+ */
+unsigned int _pow(unsigned int n, unsigned int p)
+{
+	unsigned int i, temp, r = 1;
+
+	for (i = 1; i <= p; i++)
+	{
+		temp = n;
+		r *= temp;
+	}
+	return (r);
+}
+/**
+ * binary_to_uint - check the code.
+ * @b: it's a string
+ * Return: Always 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0, len = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[len])
+		len++;
+	len--;
+	while (len)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		if (b[len] == '1')
+		{
+			decimal += _pow(2, len);
+		}
+		len--;
+	}
+	if (len == 0)
+	{
+		if (b[len] == '1')
+			decimal += 1;
+	}
+	return (decimal);
+}
