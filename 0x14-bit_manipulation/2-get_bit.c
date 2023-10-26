8ec73fd14985794a0fc9e64544b4d65eb55ee2b5
ShroukOuda <s36568961@gmail.com>
2023-10-26 11:42:26 -0700

    commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * get_bit - check the code.
+ * @n: it's n
+ * @index: it's index
+ * Return: Always 0.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (n)
+	{
+		if (n == 0 && index == 0)
+			return (0);
+		if (i == index)
+		{
+			if ((n & 1) == 0)
+				return (0);
+			else
+				return (1);
+		}
+		i++;
+		n >>= 1;
+	}
+	return (-1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,6 @@ unsigned int _pow(unsigned int n, unsigned int p);
 unsigned int binary_to_uint(const char *b);
 void binary(unsigned long int n);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif /* MAIN_H */
