4840450b16574c25b8996afde4d8b54a98847d86
shrouk_ouda <shroukouda2@gmail.com>
2024-02-04 03:32:57 -0500

    commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,12 +24,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || !strcmp(key, ""))
 		return (0);
-	index = key_index((const unsigned char*)key, ht->size);
+	index = key_index((const unsigned char) *key, ht->size);
 	newnode = malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
 		return (0);
-	newnode->key = strdup((char*)key);
-	newnode->value = strdup((char*)value);
+	newnode->key = strdup((char) *key);
+	newnode->value = strdup((char) *value);
 	newnode->next = NULL;
 	if (ht->array[index] == NULL)
 		ht->array[index] = newnode;
